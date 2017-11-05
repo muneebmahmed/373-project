2f4a19507af807d87aed555dcf358babb1c06483
Muneeb Ahmed <muneebmahmed@gmail.com>
2017-11-04 20:14:21 -0700

    Add GUI outline

---
+++ src/boardgame/play/GUI.java
@@ -0,0 +1,70 @@
+package boardgame.play;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+import boardgame.data.Command;
+import boardgame.pieces.*;
+
+public class GUI extends JFrame implements UserInterface {
+
+	//add JPanel and components here
+	
+	private volatile Piece moving;
+	private volatile Square origin;
+	private volatile Square destination;
+	
+	public GUI() throws HeadlessException {
+		// TODO Auto-generated constructor stub
+		super("Chess");
+		setSize(1000, 1000);
+		setLayout(new BorderLayout());
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		moving = null;
+		origin = null;
+		destination = null;
+		
+		setVisible(true);
+	}
+
+	public GUI(GraphicsConfiguration gc) {
+		super(gc);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GUI(String title) throws HeadlessException {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GUI(String title, GraphicsConfiguration gc) {
+		super(title, gc);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Command getCommand(Player player, Board b) {
+		// TODO Auto-generated method stub
+		destination = null;
+		
+		/*
+		 * The following code waits until the player selects a move
+		 * The player should be able to select a piece and destination with the GUI
+		 * The action listeners associated with the pieces will execute on a different thread
+		 * Therefore, the current thread should sleep to not waste resources
+		 */
+		while (destination == null) {
+			try {
+				Thread.sleep(1000);
+			}catch (InterruptedException e) {
+				//do stuff?
+			}
+		}
+		//System.out.println("Loop exited");
+		return new Command(moving, origin, destination);
+	}
+
+}
