960bbe3b568ed380fb3be237b43f65fd3773cdc4
Brock Berube <bberube@email.arizona.edu>
2017-11-28 18:03:37 +0000

    Began implementation of the start and end menus

---
+++ src/boardgame/play/GameRunner.java
@@ -0,0 +1,25 @@
+package boardgame.play;
+
+import boardgame.pieces.Board;
+import boardgame.play.*;
+
+
+public class GameRunner {
+	
+	
+	public static void main(String[] args) {
+		
+		StartMenuGUI s = new StartMenuGUI();
+		
+		
+		while( /*game is still running*/ ) {
+		
+		
+		
+		}
+		
+		EndMenuGUI e = new EndMenuGUI();
+		
+	}
+
+}
